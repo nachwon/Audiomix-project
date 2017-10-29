5ebf09829daab5e34dd9523219ab91992f923b0b
Che1 <nachwon@gmail.com>
2017-10-29 22:12:54 +0900

    mix prototype made

--- Audiomix/mixer/models.py
+++ Audiomix/mixer/models.py
@@ -1,3 +1,4 @@
 from django.db import models
 
-
+class Audio(models.Model):
+    track = models.FileField(upload_to='tracks/')
--- Audiomix/mixer/views.py
+++ Audiomix/mixer/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from pydub import AudioSegment
+from mixer.models import Audio
+
 
 def mixer(request):
-
+    if request.method == 'POST':
+        track_1 = AudioSegment.from_mp3(request.FILES['track-1'])
+        track_2 = AudioSegment.from_mp3(request.FILES['track-2'])
+        output = track_1.overlay(track_2)
+        output.export('media/mixed.mp3')
+        # Audio.objects.create(track=output)
+        # context = {
+        #     'audio': audio,
+        # }
     return render(request, 'mixer.html')
