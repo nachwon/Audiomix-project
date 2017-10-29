c2db801b31bea3ae330c022865d852476f0be452
Che1 <nachwon@gmail.com>
2017-10-29 21:23:22 +0900

    mixer view started

---
+++ Audiomix/mixer/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MixerConfig(AppConfig):
+    name = 'mixer'
---
+++ Audiomix/mixer/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+def mixer(request):
+
+    return render(request, 'mixer.html')
