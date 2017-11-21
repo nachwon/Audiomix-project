3516689f8a84d19f924465982daa03e64ce12fc9
Che1 <nachwon@gmail.com>
2017-11-22 00:08:51 +0900

    project reboot

--- Audiomix/mixer/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from mixer.models import Post, Commit
-
-admin.site.register(Post)
-admin.site.register(Commit)
--- Audiomix/mixer/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MixerConfig(AppConfig):
-    name = 'mixer'
