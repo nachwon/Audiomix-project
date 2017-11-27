668b9a14f47744da936c81dcb318cf9f0f38ab29
Che1 <nachwon@gmail.com>
2017-11-28 02:56:10 +0900

    minor refactor on users.views

--- soundhub/users/views.py
+++ soundhub/users/views.py
@@ -1,15 +1,12 @@
-from django.shortcuts import render
 from rest_framework import generics
-from rest_framework import permissions
 
 from users.models import User
 from users.serializers import UserSerializer, SignupSerializer
 
-
-# 사용자 본인 계정 조회, 수정, 삭제
 from utils.permissions import IsOwnerOrReadOnly
 
 
+# 사용자 본인 계정 조회, 수정, 삭제
 class UserDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
