08b16ea30521b660513f21c8df657a8dc74a8822
Che1 <nachwon@gmail.com>
2017-11-28 02:38:24 +0900

    aws s3 access settings added

--- soundhub/config/settings.py
+++ soundhub/config/settings.py
@@ -31,11 +31,13 @@ STATICFILES_STORAGE = 'config.storages.StaticStorage'
 MEDIAFILES_LOCATION = 'media'
 STATICFILES_LOCATION = 'static'
 
-# AWS Access
+# AWS S3 Access
 config_secret = json.loads(open(CONFIG_SETTINGS_COMMON_FILE).read())
 AWS_ACCESS_KEY_ID = config_secret['aws']['access_key_id']
 AWS_SECRET_ACCESS_KEY = config_secret['aws']['secret_access_key']
 AWS_STORAGE_BUCKET_NAME = config_secret['aws']['s3_bucket_name']
+AWS_S3_SIGNATURE_VERSION = 's3v4'
+AWS_S3_REGION_NAME = 'ap-northeast-2'
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
