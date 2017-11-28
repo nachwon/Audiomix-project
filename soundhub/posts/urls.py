848ead67f33fcf984fb7c2abb6076529c4788356
Che1 <nachwon@gmail.com>
2017-11-28 16:38:13 +0900

    post detail APIView created

--- soundhub/posts/urls.py
+++ soundhub/posts/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 
-from posts.views import PostList
+from posts.views import PostList, PostDetail
 
 urlpatterns = [
     # Post
     url(r'^list/$', PostList.as_view(), name='post-list'),
+    url(r'(?P<pk>\d+)/$', PostDetail.as_view(), name='post-detail'),
 ]
--- soundhub/posts/views.py
+++ soundhub/posts/views.py
@@ -4,6 +4,8 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from posts.models import Post
 from posts.serializers import PostSerializer
 
+from utils.permissions import IsOwnerOrReadOnly
+
 
 # 포스트 목록 조회 및 포스트 생성 API
 class PostList(generics.ListCreateAPIView):
@@ -14,3 +16,10 @@ class PostList(generics.ListCreateAPIView):
     )
 
 
+# 단일 포스트 조회, 수정, 삭제 API
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (
+        IsOwnerOrReadOnly,
+    )
