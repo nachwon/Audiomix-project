b3396170b68ec358148b6ae7fe5605556bb60030
Che1 <nachwon@gmail.com>
2017-11-28 20:42:05 +0900

    IsAuthorOrReadOnly permission added

--- soundhub/posts/views.py
+++ soundhub/posts/views.py
@@ -4,7 +4,7 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from posts.models import Post, CommentTrack
 from posts.serializers import PostSerializer, CommentTrackSerializer
 
-from utils.permissions import IsOwnerOrReadOnly
+from utils.permissions import IsOwnerOrReadOnly, IsAuthorOrReadOnly
 
 
 # 포스트 목록 조회 및 포스트 생성 API
@@ -12,6 +12,7 @@ class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = (
+        # 회원인 경우만 포스트 작성 가능
         IsAuthenticatedOrReadOnly,
     )
 
@@ -24,7 +25,8 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = (
-        IsOwnerOrReadOnly,
+        # 작성자인 경우만 포스트 수정, 삭제 가능
+        IsAuthorOrReadOnly,
     )
 
 
@@ -32,12 +34,15 @@ class CommentTrackList(generics.ListCreateAPIView):
     queryset = CommentTrack.objects.all()
     serializer_class = CommentTrackSerializer
     permission_classes = (
+        # 회원인 경우만 코멘트 작성 가능
         IsAuthenticatedOrReadOnly,
     )
 
     def perform_create(self, serializer):
+        post = serializer.get_object()
+        print(post)
         serializer.save(
             author=self.request.user,
-            post=self.post,
+            # post=self.post,
         )
 
