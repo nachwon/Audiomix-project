c7e10d53a4e3f6a4ceb418a876ea15e55cee1207
Che1 <nachwon@gmail.com>
2017-11-28 15:51:20 +0900

    postlist api view defined

--- soundhub/posts/views.py
+++ soundhub/posts/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from posts.models import Post
+
+
+# 포스트 목록 조회 및 포스트 생성 API
+from posts.serializers import PostSerializer
+from utils.permissions import IsOwnerOrReadOnly
+
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (
+        IsOwnerOrReadOnly,
+    )
