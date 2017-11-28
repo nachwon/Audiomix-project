009638e79fea4bb13a5f1cfce3d3fc180404cca2
Che1 <nachwon@gmail.com>
2017-11-28 16:29:31 +0900

    Post API shows nickname instead of user id on author field

--- soundhub/posts/serializers.py
+++ soundhub/posts/serializers.py
@@ -4,6 +4,8 @@ from posts.models import Post
 
 
 class PostSerializer(serializers.ModelSerializer):
+    author = serializers.ReadOnlyField(source='author.nickname')
+
     class Meta:
         model = Post
         fields = (
