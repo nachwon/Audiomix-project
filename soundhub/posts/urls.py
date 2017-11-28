bdb49f6ea7815e3dc89c15c406b0d44e8c407ac6
Che1 <nachwon@gmail.com>
2017-11-28 23:21:56 +0900

    url refined

--- soundhub/posts/urls.py
+++ soundhub/posts/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     url(r'(?P<pk>\d+)/$', PostDetail.as_view(), name='post-detail'),
 
     # Comment_track
-    url(r'(?P<pk>\d+)/ct-list/$', CommentTrackList.as_view(), name='comment-track-list')
+    url(r'(?P<pk>\d+)/comment/$', CommentTrackList.as_view(), name='comment-track-list'),
 ]
