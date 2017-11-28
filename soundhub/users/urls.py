912fd13057052a2724fec369d4fb6a5c4dc10e9b
Kim Jooeon <joo2theeon@gmail.com>
2017-11-29 08:22:50 +0900

    url 수정

--- soundhub/users/urls.py
+++ soundhub/users/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import url
 
+from users import apis
 from users.views import UserDetail, UserSignup
 
 urlpatterns = [
     url(r'^(?P<pk>\d+)/$', UserDetail.as_view(), name='user-detail'),
-    url(r'^signup/$', UserSignup.as_view(), name='user-signup'),
+    # url(r'^signup/$', UserSignup.as_view(), name='user-signup'),
+    url(r'^login/$', apis.Login.as_view(), name='login'),
+    url(r'^signup/$', apis.Signup.as_view(), name='signup'),
+    url(r'^activate/$', apis.ActivateUser.as_view(), name='activate'),
 ]
