22d641e209e753f1245724a7fd027bb9672379ec
Che1 <nachwon@gmail.com>
2017-11-30 00:00:25 +0900

    post list api shows all the related information

--- soundhub/posts/serializers.py
+++ soundhub/posts/serializers.py
@@ -7,12 +7,6 @@ from users.serializers import UserSerializer
 class PostSerializer(serializers.ModelSerializer):
     # 유저 시리얼라이저를 통해 유저 객체 직렬화 후 할당
     author = UserSerializer(read_only=True)
-    # 커멘트 트랙들의 정보를 표시
-    comment_tracks = serializers.SlugRelatedField(
-        many=True,
-        read_only=True,
-        slug_field='instrument'
-    )
 
     class Meta:
         model = Post
@@ -24,6 +18,8 @@ class PostSerializer(serializers.ModelSerializer):
             'author_track',
             'comment_tracks',
         )
+
+        depth = 1
 
 
 class CommentTrackSerializer(serializers.ModelSerializer):
