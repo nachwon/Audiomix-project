2d6a8ad7ea3c16a279b375dd474ea11a7c3abea7
Che1 <nachwon@gmail.com>
2017-11-29 10:12:47 +0900

    user login added for temporary use

--- soundhub/users/urls.py
+++ soundhub/users/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 
-from users.views import UserDetail, UserSignup
+from users.views import UserDetail, UserSignup, UserLogin
 
 urlpatterns = [
     url(r'^(?P<pk>\d+)/$', UserDetail.as_view(), name='user-detail'),
     url(r'^signup/$', UserSignup.as_view(), name='user-signup'),
+    url(r'^login/$', UserLogin.as_view(), name='user-login'),
 ]
