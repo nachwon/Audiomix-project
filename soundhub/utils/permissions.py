4d32cecc1b4437ec685f860404d14e6551cc3407
Che1 <nachwon@gmail.com>
2017-11-30 11:56:41 +0900

    permission fixed

--- soundhub/utils/permissions.py
+++ soundhub/utils/permissions.py
@@ -18,10 +18,12 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
 class IsAuthorOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         # 안전한 요청인 경우 True
+        # print(request.method)
+        print(view.get_object().author, request.user)
         if request.method in permissions.SAFE_METHODS:
             return True
 
         # 안전한 요청이 아닐 경우 해당 포스트의 작성자와 현재 로그인한 유저의 이메일이 일치하는지 확인
-        return view.get_object().author.email == request.user.get_full_name()
+        return view.get_object().author == request.user
 
 
