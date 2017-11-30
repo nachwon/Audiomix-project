bfa04d7585e72da78c104141bf302981c95ae3ff
Che1 <nachwon@gmail.com>
2017-11-30 13:00:44 +0900

    initial test

--- soundhub/posts/tests.py
+++ soundhub/posts/tests.py
@@ -1,3 +1,11 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from rest_framework.test import APILiveServerTestCase
 
-# Create your tests here.
+User = get_user_model()
+
+
+class PostListViewTest(APILiveServerTestCase):
+    @staticmethod
+    def create_user(username='testuser@test.co.kr'):
+        return User.objects.create_user(username=username, nickname='testuser')
