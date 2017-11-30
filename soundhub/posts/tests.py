2845117b7395fed32c3c1e18269ea19fecebc878
Che1 <nachwon@gmail.com>
2017-11-30 14:28:51 +0900

    post url names changed

--- soundhub/posts/tests.py
+++ soundhub/posts/tests.py
@@ -6,6 +6,11 @@ User = get_user_model()
 
 
 class PostListViewTest(APILiveServerTestCase):
+    API_VIEW_URL = '/post/'
+    API_VIEW_URL_NAME = 'post:list'
+
     @staticmethod
     def create_user(username='testuser@test.co.kr'):
         return User.objects.create_user(username=username, nickname='testuser')
+
+    def test_post_list_retrieve(self):
--- soundhub/posts/urls.py
+++ soundhub/posts/urls.py
@@ -4,8 +4,8 @@ from posts.views import PostList, PostDetail, CommentTrackList, CommentTrackDeta
 
 urlpatterns = [
     # Post
-    url(r'^$', PostList.as_view(), name='post-list'),
-    url(r'^(?P<pk>\d+)/$', PostDetail.as_view(), name='post-detail'),
+    url(r'^$', PostList.as_view(), name='list'),
+    url(r'^(?P<pk>\d+)/$', PostDetail.as_view(), name='detail'),
 
     # Comment_track
     url(r'^(?P<pk>\d+)/comment/$', CommentTrackList.as_view(), name='comment-track-list'),
