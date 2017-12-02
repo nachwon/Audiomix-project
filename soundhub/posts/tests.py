7538d679855873c52b59d754eced6f96ea2ce1ed
Che1 <nachwon@gmail.com>
2017-12-02 18:05:27 +0900

    test_post_list_url_name_reverse added

--- soundhub/posts/tests.py
+++ soundhub/posts/tests.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
-from django.urls import resolve
+from django.urls import resolve, reverse
 from rest_framework.test import APILiveServerTestCase
 
 from posts.views import PostList
@@ -8,17 +8,19 @@ from posts.views import PostList
 User = get_user_model()
 
 
-class PostListViewTest(APILiveServerTestCase):
+class PostListAPIViewTest(APILiveServerTestCase):
     API_VIEW_URL = '/post/'
     API_VIEW_URL_NAME = 'post:list'
     VIEW_CLASS = PostList
 
-    @staticmethod
-    def create_user(username='testuser@test.co.kr'):
-        return User.objects.create_user(username=username, nickname='testuser')
-
-    # PostList URL로 접속했을 때 PostList 뷰를 사용하고 있는지 테스트
+    # /post/로 접속했을 때 PostList 뷰를 사용하고 있는지 테스트
     def test_post_list_url_resolve(self):
         resolve_match = resolve(self.API_VIEW_URL)
         self.assertEqual(resolve_match.view_name, self.API_VIEW_URL_NAME)
         self.assertEqual(resolve_match.func.view_class, self.VIEW_CLASS)
+
+    # post:list 을 호출했을 때 /post/ url으로 연결되는지 테스트
+    def test_post_list_url_name_reverse(self):
+        url = reverse(self.API_VIEW_URL_NAME)
+        self.assertEqual(url, self.API_VIEW_URL)
+
