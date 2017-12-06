c1415be8431ed49fc1d11e44e77cbd9224b0748e
Che1 <nachwon@gmail.com>
2017-12-06 15:36:35 +0900

    serializer error fixed many=false

--- soundhub/posts/serializers.py
+++ soundhub/posts/serializers.py
@@ -86,7 +86,7 @@ class PostListSerializer(serializers.ModelSerializer):
 
 class PostDetailSerializer(serializers.ModelSerializer):
     # 유저 시리얼라이저를 통해 유저 객체 직렬화 후 할당
-    author = UserSerializer(read_only=True, many=True)
+    author = UserSerializer(read_only=True)
     author_track = serializers.FileField(max_length=255, use_url=False)
     liked = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
     comment_tracks = CommentTrackField(read_only=True)
