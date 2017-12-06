0b432d4171c93c986a02cc29287cd2fdb80713d4
Che1 <nachwon@gmail.com>
2017-12-06 21:05:55 +0900

    instrument home page view added

--- soundhub/homepages/views.py
+++ soundhub/homepages/views.py
@@ -49,8 +49,32 @@ class GenreHomePageView(ListModelMixin, generics.GenericAPIView):
         recent_post_serializer = self.post_serializer(recent_post_queryset, many=True)
 
         data = {
-            "pop_post": pop_post_serializer.data,
-            "recent_post": recent_post_serializer.data
+            "pop_posts": pop_post_serializer.data,
+            "recent_posts": recent_post_serializer.data
+        }
+        return Response(data)
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+
+class InstrumentHomePageView(ListModelMixin, generics.GenericAPIView):
+    post_serializer = PostListSerializer
+    lookup_url_kwarg = 'instrument'
+
+    def list(self, request, *args, **kwargs):
+        instrument = self.kwargs.get(self.lookup_url_kwarg)
+        post_queryset = Post.objects.filter(instrument=instrument)
+
+        pop_post_queryset = post_queryset.order_by('-num_liked')[:15]
+        recent_post_queryset = post_queryset.order_by('-created_date')[:15]
+
+        pop_post_serializer = self.post_serializer(pop_post_queryset, many=True)
+        recent_post_serializer = self.post_serializer(recent_post_queryset, many=True)
+
+        data = {
+            "pop_posts": pop_post_serializer.data,
+            "recent_posts": recent_post_serializer.data
         }
         return Response(data)
 
