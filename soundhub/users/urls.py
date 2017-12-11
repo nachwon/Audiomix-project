b024a5d9b461d06b39a70ae756fa7d6380c1a3f2
Kim Jooeon <joo2theeon@gmail.com>
2017-12-11 13:57:07 +0900

    [users/urls] google login url

--- soundhub/users/urls.py
+++ soundhub/users/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     url(r'^login/$', apis.Login.as_view(), name='login'),
     url(r'^signup/$', apis.Signup.as_view(), name='signup'),
     url(r'^activate/$', apis.ActivateUser.as_view(), name='activate'),
+    url(r'^google_login/$', apis.GoogleLogin.as_view(), name='google_login'),
 ]
