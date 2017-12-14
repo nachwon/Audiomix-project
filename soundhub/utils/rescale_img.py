e0e61e522e26eb693e57612e60758c6cbdd9cf28
Che1 <nachwon@gmail.com>
2017-12-15 00:32:28 +0900

    utils.make_profile_img added

--- soundhub/utils/rescale_img.py
+++ soundhub/utils/rescale_img.py
@@ -1,8 +1,12 @@
+import os
 from PIL import Image
+
+from django.conf import settings
+
 
 """
 프로필 이미지
-200 x 200
+200*200, 400*400
 
 프로필 배경 이미지
 750 x 422, 
@@ -12,14 +16,56 @@ from PIL import Image
 """
 
 
-def rescale(user, width, height, force=False):
+# 프로필 이미지 파일 생성
+def make_profile_img(user):
     profile_img = user.profile_img
+    sizes = ((200, 200), (400, 400), (600, 600))
+    try:
+        img = Image.open(profile_img)
+    except ValueError:
+        return None
 
-    img = Image.open(profile_img)
+    # profile_img 생성을 위한 로컬 경로
+    directory = os.path.join(settings.MEDIA_ROOT, f'user_{user.pk}/profile-img')
+    # 경로가 없으면 만들어줌
+    if not os.path.exists(directory):
+        os.makedirs(directory)
 
-    if not force:
-        img.thumbnail((width, height), Image.ANTIALIAS)
+    img_list = list()
+    if img.width == img.height:
+        for size in sizes:
+            img_copy = img.copy()
+            img_copy.thumbnail(size, Image.ANTIALIAS)
+            filename = f'profile_img_{size[0]}.png'
+            profile_dir = os.path.join(directory, filename)
+            img_copy.save(profile_dir)
+            img_list.append(profile_dir)
 
-    filename = 'hello.png'
-    img.save(filename)
-    return data
+    else:
+        src_width, src_height = img.size
+        src_ratio = float(src_width) / float(src_height)
+        for size in sizes:
+            dst_width, dst_height = size
+            dst_ratio = float(dst_width) / float(dst_height)
+
+            if dst_ratio < src_ratio:
+                crop_height = src_height
+                crop_width = crop_height * dst_ratio
+                x_offset = int(src_width - crop_width) // 2
+                y_offset = 0
+            else:
+                crop_width = src_width
+                crop_height = crop_width / dst_ratio
+                x_offset = 0
+                y_offset = int(src_height - crop_height) // 3
+            img_copy = img.copy()
+            cropped = img_copy.crop((x_offset,
+                                     y_offset,
+                                     x_offset + int(crop_width),
+                                     y_offset + int(crop_height)))
+            resized = cropped.resize((dst_width, dst_height), Image.ANTIALIAS)
+            filename = f'profile_img_{size[0]}.png'
+            profile_dir = os.path.join(directory, filename)
+            resized.save(profile_dir)
+            img_list.append(profile_dir)
+    return img_list
