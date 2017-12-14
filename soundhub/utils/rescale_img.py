a5e94d75d4b30d885e3b85fd881eea263c02187b
Che1 <nachwon@gmail.com>
2017-12-14 22:43:12 +0900

    img rescale

--- soundhub/utils/rescale_img.py
+++ soundhub/utils/rescale_img.py
@@ -1,15 +1,25 @@
-from io import BytesIO
 from PIL import Image
 
+"""
+프로필 이미지
+200 x 200
 
-def rescale(data, width, height, force=False):
-    data = BytesIO(data.read())
-    img = Image.open(data)
+프로필 배경 이미지
+750 x 422, 
+
+앨범커버
+750 x 750
+"""
+
+
+def rescale(user, width, height, force=False):
+    profile_img = user.profile_img
+
+    img = Image.open(profile_img)
 
     if not force:
         img.thumbnail((width, height), Image.ANTIALIAS)
 
-    image_file = BytesIO()
-    img.save(image_file, 'JPEG')
-    data.file = image_file
+    filename = 'hello.png'
+    img.save(filename)
     return data
