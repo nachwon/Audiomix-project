614c8619fb35fb9e6a590ff976226912fb0d3864
Che1 <nachwon@gmail.com>
2017-12-15 14:51:33 +0900

    hotfix

--- soundhub/config/celery.py
+++ soundhub/config/celery.py
@@ -1,7 +1,7 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.local')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.deploy')
 
 from django.conf import settings
 
