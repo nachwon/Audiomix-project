e254e902f3ea18d0758b05baabc1a63857fa9171
Che1 <nachwon@gmail.com>
2017-12-16 23:55:20 +0900

    local settings

--- soundhub/config/settings/base.py
+++ soundhub/config/settings/base.py
@@ -76,12 +76,12 @@ SECRET_KEY = config_secret['django']['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-RAVEN_CONFIG = {
-    'dsn': 'https://e01ad1cebe374afba306dd30c0c95aec:f188b0f719e24508946c5ab72b4de3c8@sentry.io/259770',
-    # If you are using git, you can also automatically configure the
-    # release based on the git info.
-    'release': raven.fetch_git_sha(os.path.abspath(os.pardir)),
-}
+# RAVEN_CONFIG = {
+#     'dsn': 'https://e01ad1cebe374afba306dd30c0c95aec:f188b0f719e24508946c5ab72b4de3c8@sentry.io/259770',
+#     # If you are using git, you can also automatically configure the
+#     # release based on the git info.
+#     'release': raven.fetch_git_sha(os.path.abspath(os.pardir)),
+# }
 
 ALLOWED_HOSTS = [
     'localhost',
