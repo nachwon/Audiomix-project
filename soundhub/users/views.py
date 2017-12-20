8bf54f53701fffa72c47d8d1979730deab8b1015
Kim Jooeon <joo2theeon@gmail.com>
2017-12-21 05:30:38 +0900

    [users/views] add ResetPassword view, 주석, Custom Exception, 예외처리

--- soundhub/users/views.py
+++ soundhub/users/views.py
@@ -10,18 +10,23 @@ from google.auth.transport import requests as google_requests
 from django.contrib.auth import get_user_model, authenticate
 from rest_framework import status, generics
 from rest_framework.authtoken.models import Token
-from rest_framework.exceptions import APIException
 from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from config.settings import ENCRYPTION_KEY
+from users.exceptions import (
+    RequestDataDoesNotExist,
+    UniqueFieldDuplication,
+    RequestDataInvalid,
+)
 from utils.permissions import IsOwnerOrReadOnly
 from utils.tasks.mail import (
     send_verification_mail,
     send_confirm_readmission_mail,
     send_verification_mail_after_social_login,
+    send_password_reset_mail,
 )
 from utils.encryption import encrypt, decrypt
 from .models import ActivationKeyInfo, Relationship
@@ -49,9 +54,8 @@ class UserList(generics.ListAPIView):
     )
 
 
-# 로그인
 class Login(APIView):
-    def post(self, request, *args, **kwargs):
+    def post(self, request):
         email = request.data['email']
         password = request.data['password']
 
@@ -70,7 +74,6 @@ class Login(APIView):
             return Response(data, status=status.HTTP_401_UNAUTHORIZED)
 
 
-# 회원가입
 class Signup(APIView):
     def get(self, request):
         """
@@ -89,7 +92,7 @@ class Signup(APIView):
         :return: None
         """
         # get parameter 에서 값 추출
-        # 암호화된 activation key 와
+        # 암호화된 activation key 와 password 복호화
         activation_key = decrypt(
             key=ENCRYPTION_KEY,
             encrypted_text=request.GET['activation_key'],
@@ -105,7 +108,7 @@ class Signup(APIView):
         activation_key_info = get_object_or_404(ActivationKeyInfo, key=activation_key)
         # activation key 가 만료된 경우
         if not activation_key_info.expires_at > timezone.now():
-            raise APIException('activation_key 의 기한이 만료되었습니다.')
+            raise RequestDataInvalid('activation_key 의 기한이 만료되었습니다.')
 
         # 해당 유저 정보를 변경하고 저장
         user = activation_key_info.user
@@ -127,10 +130,10 @@ class Signup(APIView):
                 - 추가 회원가입 링크를 담은 인증 메일 발송
             2) Soundhub 계정일 때
                 a. is_active 가 True
-                    - APIException: '이미 존재하는 유저입니다'
+                    - RequestDataInvalid: '이미 존재하는 유저입니다'
                 b. is_active 가 False
                     - 안내 메일 발송
-                    - APIException: '이메일 인증 중인 유저입니다. 메일을 확인해주세요.'
+                    - RequestDataInvalid: '이메일 인증 중인 유저입니다. 메일을 확인해주세요.'
 
         2. 전달된 email 의 유저가 존재하지 않는 경우
             - User, ActivationKeyInfo 생성
@@ -180,12 +183,10 @@ class Signup(APIView):
                 send_verification_mail_after_social_login.delay(data, [user.email])
             # Soundhub 계정일 때
             elif user.is_active is True:
-                raise APIException('이미 존재하는 유저입니다')
+                raise RequestDataInvalid('이미 존재하는 유저입니다')
             elif user.is_active is False:
                 send_confirm_readmission_mail.delay([user.email])
-                raise APIException('이메일 인증 중인 유저입니다. 메일을 확인해주세요.')
-
-        print(request.data.get('profile_img', None))
+                raise RequestDataInvalid('이메일 인증 중인 유저입니다. 메일을 확인해주세요.')
 
         serializer = SignupSerializer(data=request.data)
         if serializer.is_valid():
@@ -229,8 +230,18 @@ class GoogleLogin(APIView):
             "locale": "en"
         }
         :param request:
-        :return:
+            data = {
+                'token': Google token,
+                'client_id': Google Client ID,
+            }
+        :return: Response(user 객체, token)
         """
+        # Request Data 검사
+        if 'token' not in request.data:
+            raise RequestDataDoesNotExist('토큰이 없습니다')
+        if 'client_id' not in request.data:
+            raise RequestDataDoesNotExist('Google Client ID 가 없습니다')
+
         token = request.data['token']
         client_id = request.data['client_id']
 
@@ -239,9 +250,9 @@ class GoogleLogin(APIView):
             id_info = id_token.verify_oauth2_token(token, google_requests.Request(), client_id)
             # token 발행정보 확인
             if id_info['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
-                raise ValueError('Wrong issuer.')
-        except ValueError:
-            raise APIException('token 또는 client_id 가 유효하지 않습니다')
+                raise RequestDataInvalid('토큰이 유효하지 않습니다')
+        except RequestDataInvalid:
+            raise RequestDataInvalid('token 또는 client_id 가 유효하지 않습니다')
 
         # 이미 존재하는 유저일 경우 유저 생성 없이 기존 유저 반환
         if User.objects.filter(email=id_info['email']).exists():
@@ -256,12 +267,14 @@ class GoogleLogin(APIView):
             return Response(data, status=status.HTTP_200_OK)
 
         # 존재하지 않는 유저일 경우만 받음
+        if 'nickname' not in request.data:
+            raise RequestDataDoesNotExist('nickname 이 없습니다')
         nickname = request.data['nickname']
-        instrument = request.data['instrument']
+        instrument = request.data['instrument'] if 'instrument' in request.data else None
 
         # 닉네임이 존재할 경우
         if User.objects.filter(nickname=nickname).exists():
-            raise APIException('이미 존재하는 닉네임입니다')
+            raise UniqueFieldDuplication('이미 존재하는 닉네임입니다')
         else:
             # 토큰 정보로 유저 생성. 이메일 인증 생략하고 바로 is_active=True
             user = User(
@@ -283,94 +296,18 @@ class GoogleLogin(APIView):
 
 
 class FacebookLogin(APIView):
-    # access token 값을 저장하는 클래스
-    class AccessTokenInfo(NamedTuple):
-        access_token: str
-        token_type: str
-        expires_in: str
-
-    # token 값의 유효성을 검사하기 위한 정보를 저장하는 클래스
-    class DebugTokenInfo(NamedTuple):
-        app_id: str
-        application: str
-        expires_at: int
-        is_valid: bool
-        issued_at: int
-        scopes: list
-        type: str
-        user_id: str
-
-    # User 정보를 담는 클래스
-    class UserInfo:
-        def __init__(self, data):
-            self.id = data['id']
-            self.email = data.get('email', '')
-            self.url_picture = data['picture']['data']['url']
-
-    # GET 요청이 들어왔을 때.
-    def get(self, request):
-        # http://developers.facebook.com/ 에서 만든 Facebook 애플리케이션 access 정보.
-        app_id = settings.FACEBOOK_APP_ID
-        app_secret_code = settings.FACEBOOK_APP_SECRET_CODE
-        app_access_token = f'{app_id}|{app_secret_code}'
-        code = request.GET['code']
-
-        # token 을 받아오는 메서드
-        def get_access_token_info(code):
-            params = {
-                'client_id': app_id,
-                'client_secret': app_secret_code,
-                'redirect_uri': '{scheme}://{host}{redirect_url}'.format(
-                    scheme=request.scheme,
-                    host=request.META['HTTP_HOST'],
-                    redirect_url=reverse('homepage')
-                ),
-                'code': code,
-            }
-            response = requests.get('https://graph.facebook.com/v2.10/oauth/access_token', params=params)
-
-            # **response.json()에 대하여
-            # 1. json.loads(response.content) 와 같음
-            # 2. 앞에 붙은 **는, json 정보를 key-value 로 집어 넣는 것을 의미한다.
-            # AccessTokenInfo(access_token=response.json()['access_token'],
-            #    'token_type'=response.json()['token_type.....
-            return self.AccessTokenInfo(**response.json())
-
-        # 받아온 토큰 값이 진짜 토큰인지 확인하는 메서드
-        def get_debug_token_info(access_token):
-            params = {
-                'input_token': access_token,
-                'access_token': app_access_token,
-            }
-            response = requests.get('https://graph.facebook.com/debug_token', params=params)
-            return self.DebugTokenInfo(**response.json()['data'])
-
-        # 토큰을 받아오고 디버그하는 메서드 실행
-        token_info = get_access_token_info(code)
-        access_token = token_info.access_token
-        debug_token_info = get_debug_token_info(token_info.access_token)
-
-        # 토큰을 사용해서 유저 정보 받아옴
-        url_graph_user_info = 'https://graph.facebook.com/me'
-        user_information_fields = ['id', 'name', 'picture', 'email']
-        params_graph = {
-            'fields': ','.join(user_information_fields),
-            'access_token': access_token,
-        }
-        response = requests.get(url_graph_user_info, params_graph)
-        result = response.json()
-        user_info = self.UserInfo(data=result)
-
-        # 현재 클래스는 프론트 측의 작업이므로, 유저 생성 없이 유저 아이디와 토큰만을 전달한다.
-        data = {
-            'facebook_user_id': user_info.id,
-            'access_token': access_token,
-        }
-        return Response(data)
-
     def post(self, request):
-        # request.data 에 facebook_user_id 와 access_token 이 전달되어 옴
-        print(request.data)
+        """
+        Facebook token, facebook_user_id 를 받아서 유효성 검사 후
+        해당 토큰으로 생성된 유저가 있으면 반환, 없으면 생성
+
+        :param request:
+            data = {
+                'access_token': facebook token,
+                'facebook_user_id': facebook user id,
+            }
+        :return: Response(user 객체, token)
+        """
 
         # token 값의 유효성을 검사하기 위한 정보를 저장하는 클래스
         class DebugTokenInfo(NamedTuple):
@@ -394,18 +331,26 @@ class FacebookLogin(APIView):
                 'access_token': app_access_token,
             }
             response = requests.get('https://graph.facebook.com/debug_token', params=params)
+            if 'error' in response.json()['data']:
+                raise RequestDataInvalid('잘못된 토큰입니다')
             return DebugTokenInfo(**response.json()['data'])
 
+        # Request Data 검사
+        if 'access_token' not in request.data:
+            raise RequestDataDoesNotExist('토큰이 없습니다')
+        if 'facebook_user_id' not in request.data:
+            raise RequestDataDoesNotExist('Facebook User ID 가 없습니다')
+
+        # 토큰 유효성 검사
         debug_token_info = get_debug_token_info(request.data['access_token'])
-
         if debug_token_info.user_id != request.data['facebook_user_id']:
-            raise APIException('페이스북 토큰의 사용자와 전달받은 facebook_user_id가 일치하지 않음')
-
+            raise RequestDataInvalid('페이스북 토큰의 사용자와 전달받은 facebook_user_id가 일치하지 않음')
         if not debug_token_info.is_valid:
-            raise APIException('페이스북 토큰이 유효하지 않음')
+            raise RequestDataInvalid('페이스북 토큰이 유효하지 않음')
 
-        # FacebookBackend를 사용해서 유저 인증
+        # FacebookBackend 를 사용해서 유저 인증
         user = authenticate(facebook_user_id=request.data['facebook_user_id'])
+        # 해당 토큰으로 생성된 유저가 존재하는 경우, 해당 유저 반환
         if user:
             user.is_active = True
             user.save()
@@ -415,13 +360,21 @@ class FacebookLogin(APIView):
                 # 'is_active': user.is_active,  # 디버그용
             }
             return Response(data, status=status.HTTP_200_OK)
+        # 해당 토큰으로 생성된 유저가 없는 경우, 새 유저 생성
         else:
             try:
                 email = request.data['email']
                 nickname = request.data['nickname']
                 instrument = request.data['instrument'] if 'instrument' in request.data else None
-            except User.DoesNotExist:
-                return Response(None)
+            except RequestDataDoesNotExist:
+                raise RequestDataDoesNotExist('유저를 생성할 데이터가 없습니다')
+
+            # 이메일 또는 닉네임이 존재할 경우
+            if User.objects.filter(email=email).exists():
+                raise UniqueFieldDuplication('이미 존재하는 이메일입니다')
+            if User.objects.filter(nickname=nickname).exists():
+                raise UniqueFieldDuplication('이미 존재하는 닉네임입니다')
+
             # 인증에 실패한 경우 페이스북유저 타입으로 유저를 만들어줌
             user = User.objects.create_user(
                 email=email,
@@ -435,12 +388,7 @@ class FacebookLogin(APIView):
                 'user': UserSerializer(user).data,
                 'token': user.token,
             }
-        return Response(data)
-
-# 특정 기능을 테스트 할 때 쓰는 뷰
-# class Test(APIView):
-#     def post(self, request):
-#         return Response(None)
+        return Response(data, status=status.HTTP_201_CREATED)
 
 
 class ActivateUser(APIView):
@@ -456,7 +404,7 @@ class ActivateUser(APIView):
         activation_key_info = get_object_or_404(ActivationKeyInfo, key=activation_key)
         # activation key 가 만료된 경우
         if not activation_key_info.expires_at > timezone.now():
-            raise APIException('activation_key 의 기한이 만료되었습니다.')
+            raise RequestDataInvalid('activation_key 의 기한이 만료되었습니다.')
         # activation key 와 일치하는 정보가 있고, key 가 유효할 경우
         activation_key_info.user.is_active = True
         # user.save()
@@ -476,20 +424,121 @@ class FollowUserToggle(generics.GenericAPIView):
         IsAuthenticatedOrReadOnly,
     )
 
-    def post(self, request, *args, **kwargs):
+    def post(self, request):
         to_user_instance = self.get_object()
         from_user_instance = request.user
 
         if to_user_instance in from_user_instance.following.all():
-            relation = Relationship.objects.get(to_user_id=to_user_instance.pk,
-                                                from_user_id=from_user_instance.pk)
+            relation = Relationship.objects.get(
+                to_user_id=to_user_instance.pk,
+                from_user_id=from_user_instance.pk
+            )
             relation.delete()
         else:
-            Relationship.objects.create(to_user_id=to_user_instance.pk,
-                                        from_user_id=from_user_instance.pk)
+            Relationship.objects.create(
+                to_user_id=to_user_instance.pk,
+                from_user_id=from_user_instance.pk
+            )
 
         from_user_instance.save_num_relations()
         to_user_instance.save_num_relations()
 
         data = UserSerializer(from_user_instance).data
         return Response(data)
+
+
+class ResetPassword(APIView):
+    def get(self, request):
+        """
+        password 변경 링크를 통해서만 접근 가능
+        activation key 로 유효한 접근인지 확인 후
+        get parameter 로 전달된 정보로 password 재설정
+
+        :param request: 암호화된 activation key 와 password 정보
+        :return: Response(1)
+        """
+        try:
+            # get parameter 에서 값 추출
+            # 암호화된 activation key 와 password 복호화
+            activation_key = decrypt(
+                key=ENCRYPTION_KEY,
+                encrypted_text=request.GET['activation_key'],
+            )
+            password = decrypt(
+                key=ENCRYPTION_KEY,
+                encrypted_text=request.GET['password'],
+            )
+        except RequestDataDoesNotExist:
+            raise RequestDataDoesNotExist('잘못된 요청입니다')
+
+        # activation key 에 해당하는 유저가 존재하는지 검사
+        activation_key_info = get_object_or_404(ActivationKeyInfo, key=activation_key)
+        # activation key 가 만료된 경우
+        if not activation_key_info.expires_at > timezone.now():
+            raise RequestDataInvalid('activation_key 의 기한이 만료되었습니다.')
+
+        # password 변경
+        activation_key_info.user.set_password(password)
+        activation_key_info.user.save()
+
+        return Response(1, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        """
+        password 를 변경하기 전, 유저 본인이 맞는지 확인하기 위해 password 재검사
+
+        :param request: password
+        :return: Response(1)
+        """
+        if 'password' not in request.data:
+            raise RequestDataDoesNotExist('password 값이 없습니다')
+        password = request.data['password']
+
+        # password 가 맞는지 검사
+        if not request.user.check_password(password):
+            raise RequestDataInvalid('password 가 유효하지 않습니다')
+
+        return Response(1, status=status.HTTP_200_OK)
+
+    def put(self, request):
+        """
+        비밀번호 변경 메일 발송
+
+        :param request:
+            data = {
+                'password1': 비밀번호
+                'password2': 확인용 비밀번호
+            }
+        :return: Response(1)
+        """
+        try:
+            password1 = request.data['password1']
+            password2 = request.data['password2']
+        except RequestDataDoesNotExist:
+            raise RequestDataDoesNotExist('password1 또는 password2 값이 전달되지 않았습니다')
+
+        if password1 == password2:
+            # password 암호화
+            encrypted_password = encrypt(
+                key=ENCRYPTION_KEY,
+                plain_text=request.data['password1'],
+            )
+            # 유저의 activation key 새로 설정
+            request.user.activationkeyinfo.refresh()
+            # activation key info 암호화
+            encrypted_activation_key = encrypt(
+                key=ENCRYPTION_KEY,
+                plain_text=request.user.activationkeyinfo.key,
+            )
+
+            data = {
+                'activation_key': encrypted_activation_key,
+                'password': encrypted_password,  # password 암호화
+            }
+            # password 변경 메일 발송
+            send_password_reset_mail.delay(data, [request.user.email])
+
+            return Response(1, status=status.HTTP_200_OK)
+
+        else:
+            raise RequestDataInvalid('password1 과 password2 가 일치하지 않습니다')
