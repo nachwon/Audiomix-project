d85f0a5eb4f76fd93f7c30cba2dede70b7e7d596
Kim Jooeon <joo2theeon@gmail.com>
2017-12-21 05:35:38 +0900

    [users/tasks/mail] add send_password_reset_mail

--- soundhub/utils/tasks/mail.py
+++ soundhub/utils/tasks/mail.py
@@ -8,6 +8,7 @@ __all__ = (
     'send_verification_mail',
     'send_confirm_readmission_mail',
     'send_verification_mail_after_social_login',
+    'send_password_reset_mail',
 )
 
 
@@ -84,6 +85,42 @@ def send_verification_mail_after_social_login(data, recipient_list):
     subject = '[Soundhub] Email Verification (Signup Soundhub after social login)'
     message = ''  # 메세지는 필수 필드
     html_message = f'Verify your email to login Soundhub: <a href="{signup_link}">signup link</a>'
+    from_email = 'joo2theeon@gmail.com'
+    return send_mail(
+        subject=subject,
+        message=message,
+        html_message=html_message,
+        from_email=from_email,
+        recipient_list=recipient_list,
+    )
+
+
+@celery_app.task
+def send_password_reset_mail(data, recipient_list):
+    """
+    비밀번호가 변경되었음을 알리는 메일. 본인이 한 것이 아닐 경우, 다시 변경할 수 있는 링크가 걸려있다.
+
+    :param data: 비밀번호 변경에 필요한 정보 dictionary 객체
+    :param recipient_list: 수신자 email list 객체
+    :return: send_mail 함수 반환 값
+    """
+    scheme = 'http://'
+    # host = 'soundhub-dev.ap-northeast-2.elasticbeanstalk.com'
+    host = 'localhost:8000'
+
+    # data 에 전달된 값을 get parameter 로 재구성
+    # params = '?key=value&key=value&' 형태
+    params = '?'
+    for key, value in data.items():
+        params = params + f'{key}={value}&'
+
+    # 완성된 패스워드 변경 링크
+    reset_password_link = scheme + host + reverse('user:password') + params[:-1]  # params[:-1]은 맨 뒤에 &를 떼는 로직
+
+    subject = '[Soundhub] Reset Password Link  (Signup Soundhub after social login)'
+    message = ''  # 메세지는 필수 필드
+    html_message = f'Verify your email to reset password in Soundhub: ' \
+                   f'<a href="{reset_password_link}">reset password link</a>'
     from_email = 'joo2theeon@gmail.com'
     return send_mail(
         subject=subject,
