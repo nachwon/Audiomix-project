9438313c1536902ab84cadc5eef957af5dfa52f0
Che1 <nachwon@gmail.com>
2017-12-22 15:13:09 +0900

    hotfix

--- soundhub/config/settings/base.py
+++ soundhub/config/settings/base.py
@@ -78,6 +78,13 @@ REST_FRAMEWORK = {
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config_secret['django']['SECRET_KEY']
 
+RAVEN_CONFIG = {
+    'dsn': 'https://e01ad1cebe374afba306dd30c0c95aec:f188b0f719e24508946c5ab72b4de3c8@sentry.io/259770',
+    # If you are using git, you can also automatically configure the
+    # release based on the git info.
+    'release': raven.fetch_git_sha(os.path.abspath(os.pardir)),
+}
+
 ALLOWED_HOSTS = [
     'localhost',
     '.ap-northeast-2.elasticbeanstalk.com',
