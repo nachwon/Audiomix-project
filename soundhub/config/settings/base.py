f59555a914a1bee80290a926945bfbb56390729a
Che1 <nachwon@gmail.com>
2017-12-22 15:14:36 +0900

    hotfix

--- soundhub/config/settings/base.py
+++ soundhub/config/settings/base.py
@@ -14,6 +14,8 @@ import os
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import raven
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 # 루트 경로
 ROOT_DIR = os.path.dirname(BASE_DIR)
