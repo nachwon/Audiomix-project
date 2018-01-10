5ebb26e772fd0eb9fccceed070876c67b4befda3
Che1 <nachwon@gmail.com>
2018-01-10 10:23:51 +0900

    popup layer added

--- soundhub/homepages/views.py
+++ soundhub/homepages/views.py
@@ -8,6 +8,6 @@ User = get_user_model()
 
 def index(request):
     context = {
-        "pop_posts": Post.objects.order_by('-num_liked')[:15]
+        "pop_posts": Post.objects.order_by('-num_liked')[:10]
     }
     return render(request, 'index.html', context)
