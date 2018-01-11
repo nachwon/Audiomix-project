714c27398bcd48f88ce2d34742a8c6d73fe624d6
Che1 <nachwon@gmail.com>
2018-01-11 11:58:57 +0900

    login form css

--- soundhub/users/forms.py
+++ soundhub/users/forms.py
@@ -2,5 +2,14 @@ from django import forms
 
 
 class SignInForm(forms.Form):
-    email = forms.EmailField(max_length=255)
-    password = forms.CharField(label='Password', widget=forms.PasswordInput())
+    email = forms.EmailField(max_length=255,
+                             widget=forms.EmailInput(
+                                 attrs={
+                                     'class': 'signin-field',
+                                 }
+                             ))
+    password = forms.CharField(widget=forms.PasswordInput(
+                                   attrs={
+                                       'class': 'signin-field'
+                                   }
+                               ))
