b167afb0c873ff5df53703118fbf40f230823f3b
Che1 <nachwon@gmail.com>
2018-01-14 20:33:32 +0900

    facebook user info separated

--- soundhub/users/views.py
+++ soundhub/users/views.py
@@ -5,6 +5,8 @@ from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.urls import reverse
 
+from utils.facebook import get_facebook_user_info
+
 User = get_user_model()
 
 
@@ -20,63 +22,22 @@ def sign_in(request):
 
 
 def facebook_login(request):
-    app_id = settings.FB_APP_ID
-    app_secret = settings.FB_SECRET_CODE
+    user_info = get_facebook_user_info(request)
+    email = user_info.get('email')
+    nickname = user_info.get('name')
 
-    # 토큰 요청
-    code = request.GET['code']
-    redirect_uri = f"{request.scheme}://{request.META['HTTP_HOST']}{reverse('views:user:fb-login')}"
-    url_access_token = "https://graph.facebook.com/v2.11/oauth/access_token"
-    params_access_token = {
-        "client_id": app_id,
-        "redirect_uri": redirect_uri,
-        "client_secret": app_secret,
-        "code": code,
-    }
+    # 유저 생성
+    obj, created = User.objects.get_or_create(
+        email=email,
+        nickname=nickname,
+        user_type='F'
+    )
 
-    response = requests.get(url_access_token, params=params_access_token)
-
-    # 토큰 검사
-    access_token = response.json()['access_token']
-    url_debug_token = 'https://graph.facebook.com/debug_token'
-    params_debug_token = {
-        "input_token": access_token,
-        "access_token": f'{app_id}|{app_secret}'
-    }
-
-    debug_result = requests.get(url_debug_token, params=params_debug_token)
-
-    # 토큰이 유효하면 유저 정보 요청
-    if debug_result.json()['data']['is_valid'] is True:
-        url_user_info = 'https://graph.facebook.com/me'
-        user_info_fields = [
-            'id',  # 아이디
-            'name', # 이름
-            'picture',  # 프로필 사진
-            'email',  # 이메일
-        ]
-        params_user_info = {
-            "fields": ','.join(user_info_fields),
-            "access_token": access_token
-        }
-        result = requests.get(url_user_info, params=params_user_info)
-        user_info = result.json()
-
-        email = user_info.get('email')
-        nickname = user_info.get('name')
-
-        # 유저 생성
-        obj, created = User.objects.get_or_create(
-            email=email,
-            nickname=nickname,
-            user_type='F'
-        )
-
-        # 유저 인증 및 로그인
-        # authenticate 를 거치면 user.backend 속성에 인증에 사용된 벡엔드가 부여됨.
-        user = authenticate(email=obj.email)
-        # 여러개의 인증 벡엔드 사용 중일 경우 user.backend 속성으로 어떤 벡엔드로 인증되었는지 알려주어야 함.
-        login(request, user, backend=user.backend)
+    # 유저 인증 및 로그인
+    # authenticate 를 거치면 user.backend 속성에 인증에 사용된 벡엔드가 부여됨.
+    user = authenticate(email=obj.email)
+    # 여러개의 인증 벡엔드 사용 중일 경우 user.backend 속성으로 어떤 벡엔드로 인증되었는지 알려주어야 함.
+    login(request, user, backend=user.backend)
     return redirect('views:home')
 
 
---
+++ soundhub/utils/facebook.py
@@ -0,0 +1,49 @@
+import requests
+from django.conf import settings
+from django.urls import reverse
+
+
+def get_facebook_user_info(request):
+    app_id = settings.FB_APP_ID
+    app_secret = settings.FB_SECRET_CODE
+
+    # 토큰 요청
+    code = request.GET['code']
+    redirect_uri = f"{request.scheme}://{request.META['HTTP_HOST']}{reverse('views:user:fb-login')}"
+    url_access_token = "https://graph.facebook.com/v2.11/oauth/access_token"
+    params_access_token = {
+        "client_id": app_id,
+        "redirect_uri": redirect_uri,
+        "client_secret": app_secret,
+        "code": code,
+    }
+
+    response = requests.get(url_access_token, params=params_access_token)
+
+    # 토큰 검사
+    access_token = response.json()['access_token']
+    url_debug_token = 'https://graph.facebook.com/debug_token'
+    params_debug_token = {
+        "input_token": access_token,
+        "access_token": f'{app_id}|{app_secret}'
+    }
+
+    debug_result = requests.get(url_debug_token, params=params_debug_token)
+
+    # 토큰이 유효하면 유저 정보 요청
+    if debug_result.json()['data']['is_valid'] is True:
+        url_user_info = 'https://graph.facebook.com/me'
+        user_info_fields = [
+            'id',  # 아이디
+            'name', # 이름
+            'picture',  # 프로필 사진
+            'email',  # 이메일
+        ]
+        params_user_info = {
+            "fields": ','.join(user_info_fields),
+            "access_token": access_token
+        }
+        result = requests.get(url_user_info, params=params_user_info)
+        user_info = result.json()
+
+        return user_info
