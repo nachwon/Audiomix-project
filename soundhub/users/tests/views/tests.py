cd5cbab46c9b403e3040b713a8674405e91db6aa
Che1 <nachwon@gmail.com>
2018-01-22 12:23:01 +0900

    login test added

--- soundhub/users/tests/views/tests.py
+++ soundhub/users/tests/views/tests.py
@@ -4,6 +4,7 @@ from django.contrib.auth import get_user_model
 from django.test import Client
 
 from users.models import Genre, Instrument
+from users.views import sign_up
 
 User = get_user_model()
 
@@ -20,8 +21,8 @@ class SignupViewTest(TestCase):
     def test_sign_up_using_password(self):
         c = Client()
 
-        email = "test@test.com"
-        nickname = "testuser"
+        email = "signup_test@test.com"
+        nickname = "signup_testuser"
         password1 = "password"
         password2 = "password"
 
@@ -37,17 +38,51 @@ class SignupViewTest(TestCase):
 
         user = User.objects.get(email=email)
 
+        # View 테스트
         self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, "/home/")
+        self.assertEqual(response.resolver_match.func, sign_up)
 
-        self.assertEqual(user.email, "test@test.com")
-        self.assertEqual(user.nickname, "testuser")
+        # User 테스트
+        self.assertEqual(user.email, "signup_test@test.com")
+        self.assertEqual(user.nickname, "signup_testuser")
 
+        # User 장르 테스트
         rock = Genre.objects.get(pk=1)
         jazz = Genre.objects.get(pk=3)
 
         self.assertIn(rock, user.genre.all())
         self.assertIn(jazz, user.genre.all())
 
+        # User 악기 테스트
         guitar = Instrument.objects.get(pk=2)
 
         self.assertIn(guitar, user.instrument.all())
+
+
+class LoginViewTest(TestCase):
+    def setUp(self):
+        user = User(email="login_test@test.com",
+                    nickname="login_testuser",
+                    )
+        user.set_password("password")
+        user.save()
+
+    def test_login_using_password(self):
+        c = Client()
+
+        email = "login_test@test.com"
+        password = "password"
+
+        context = {
+            "email": email,
+            "password": password
+        }
+
+        response = c.post('/user/signin/', context)
+
+        # print(response.content)
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, "/home/")
+
