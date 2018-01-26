ded5ac93ee10e74938be7404a681b546c2b40958
Che1 <nachwon@gmail.com>
2018-01-26 23:02:43 +0900

    anonymous user like disabled

---
+++ static/js/profile/waveform.js
@@ -0,0 +1,69 @@
+// waveform 클래스를 가진 div 목록 가져옴
+var div_list = document.getElementsByClassName('waveform');
+var surfer_list = [];
+
+// 목록을 순회하면서
+for (var i = 0; i < div_list.length; i++) {
+    // div의 datasrc 속성의 값을 track_url 변수에 저장
+    var track_url = div_list[i].getAttribute('datasrc');
+    // waveform을 그려줄 각각의 div에 부여할 id값 생성
+    var waveform_id = '#waveform-' + (i + 1);
+    // waveform 객체 생성
+    var wavesurfer = WaveSurfer.create({
+        // 대상 객체 id값(필수)
+        container: waveform_id,
+        // 막대 넓이
+        barWidth: 2,
+        // 막대 높이
+        barHeight: 0.6,
+        // 커서 이전 부분 색상
+        progressColor: '#E2B026',
+        // 커서 색상
+        cursorColor: 'transparent',
+        // 커서 이후 부분 색상
+        waveColor: '#333533',
+        // 스크롤바 숨기기
+        hideScrollbar: true
+    });
+
+    // waveform 객체 로드
+    wavesurfer.load(track_url);
+    // 로드된 wavesurfer 객체를 리스트에 저장
+    surfer_list.push(wavesurfer);
+
+    //
+    wavesurfer.on("ready", function() {
+        // loader를 없애주는 함수
+        waveformLoader();
+    });
+}
+
+var play_btn_list = $(".play-btn");
+for (i = 0; i < play_btn_list.length; i++) {
+    play_btn_list[i].onclick = (function (j) {
+        return function () {
+            $(this)
+                .find('[data-fa-processed]')
+                .toggleClass('fas fa-pause-circle fa-3x')
+                .toggleClass('fas fa-play-circle fa-3x')
+
+            if (surfer_list[j].isPlaying()) {
+                surfer_list[j].pause()
+            }
+            else {
+                surfer_list[j].play()
+            }
+        }
+    }(i))
+}
+
+// loader를 없애주는 함수
+function waveformLoader() {
+    // waveform-loader라는 클래스명을 가진 객체 리스트 저장
+    var loader = document.getElementsByClassName('waveform-loader');
+
+    // 리스트를 순회하면서 display 속성을 none으로 변경
+    for (var i = 0; i < loader.length; i++) {
+        loader[i].style.display = 'none';
+    }
+}
