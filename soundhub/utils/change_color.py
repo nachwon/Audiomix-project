72e6c5d0e8acdbddac348474971232b94007989e
Che1 <nachwon@gmail.com>
2018-01-30 11:16:00 +0900

    updated

--- soundhub/utils/change_color.py
+++ soundhub/utils/change_color.py
@@ -6,18 +6,19 @@ im = Image.open(imagePath)
 
 
 # 이미지의 색을 바꿔주는 함수
-def redOrBlack (im):
+def change_color(im):
     newimdata = []
     black1 = (51, 53, 51, 255)
     black2 = (50, 52, 50, 128)
-    yellow = (226, 176, 38, 255)
+    yellow1 = (226, 176, 38, 255)
+    yellow2 = (225, 175, 37, 128)
     blank = (0, 0, 0, 0)
     for color in im.getdata():
         print(color)
         if color == black1:
-            newimdata.append(yellow)
+            newimdata.append(yellow1)
         elif color == black2:
-            newimdata.append(yellow)
+            newimdata.append(yellow2)
         else:
             newimdata.append(blank)
     newim = Image.new(im.mode, im.size)
@@ -25,4 +26,4 @@ def redOrBlack (im):
     return newim
 
 
-redOrBlack(im).save(newImagePath)
+change_color(im).save(newImagePath)
