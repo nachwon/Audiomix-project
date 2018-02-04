f883d15396a73ada73c874ffde5b97bfe87cb708
Che1 <nachwon@gmail.com>
2018-02-04 17:53:08 +0900

    middleware added

--- soundhub/config/settings/base.py
+++ soundhub/config/settings/base.py
@@ -136,6 +136,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # 'config.middleware.byterange.SimpleMiddleware'
 ]
 
 
