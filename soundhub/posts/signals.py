86c3e6293957ffc09944b28ee97c0bd180434659
Che1 <nachwon@gmail.com>
2018-02-05 15:20:11 +0900

    comments added

--- soundhub/posts/signals.py
+++ soundhub/posts/signals.py
@@ -1,7 +1,5 @@
 import os
 from django.core.files.base import ContentFile
-from django.db.models.signals import post_save, pre_save
-from django.dispatch import receiver
 
 from utils.pywave import Waveform
 
@@ -9,6 +7,7 @@ _UNSAVED_AUTHOR_TRACK = 'unsaved_author_track'
 _UNSAVED_POST_IMG = 'unsaved_post_img'
 
 
+# save 메서드가 시작될 때 author_track과 post_img를 저장하지 않고 넘어감
 def skip_saving_file(sender, instance, **kwargs):
     if not instance.pk:
         if not hasattr(instance, _UNSAVED_AUTHOR_TRACK):
@@ -19,27 +18,35 @@ def skip_saving_file(sender, instance, **kwargs):
         instance.post_img = None
 
 
+# save 메서드가 끝나고 난 이후 author_track과 post_img를 할당한 뒤 다시 save 메서드 호출
 def save_file(sender, instance, created, **kwargs):
     if created:
+        # author_track 오디오 파일 저장
         if hasattr(instance, _UNSAVED_AUTHOR_TRACK):
             instance.author_track = getattr(instance, _UNSAVED_AUTHOR_TRACK)
+
+            # author_track에 대한 웨이브폼 이미지 생성
             file = instance.author_track
             waveform = Waveform(file, file.name)
             waveform_base = waveform.save()
             waveform_cover = waveform.change_color(waveform_base)
 
+            # 웨이브폼 이미지 업로드
             with open(waveform_base, 'rb') as f1:
                 base = ContentFile(f1.read())
 
             with open(waveform_cover, 'rb') as f2:
                 cover = ContentFile(f2.read())
 
+            # 필드 저장
             instance.author_track_waveform_base.save('author_track.png', base)
             instance.author_track_waveform_cover.save('author_track_cover.png', cover)
 
+            # 로컬 파일 삭제
             os.remove(waveform_base)
             os.remove(waveform_cover)
 
+        # post_img 저장
         if hasattr(instance, _UNSAVED_POST_IMG):
             instance.post_img = getattr(instance, _UNSAVED_POST_IMG)
         instance.save()
