da251adf250f8be60ee295b7b2bd3f51eaca0ffe
Che1 <nachwon@gmail.com>
2018-02-09 09:56:43 +0900

    reefactored

--- soundhub/users/views/user.py
+++ soundhub/users/views/user.py
@@ -1,11 +1,10 @@
 import json
-from itertools import chain
 
 from django.contrib.auth import get_user_model
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.decorators.http import require_GET
 
-from posts.models import Post, PostLike
 from users.forms import SignInForm
 from users.models import Relationship
 
@@ -13,29 +12,20 @@ from users.models import Relationship
 User = get_user_model()
 
 
+@require_GET
 def user_detail(request, pk):
-    user_exists = User.objects.all().filter(pk=pk).exists()
+    user = get_object_or_404(User, pk=pk)
 
-    if request.method == 'GET' and user_exists:
-        user = User.objects.get(pk=pk)
+    user_posts = user.post_set.all()[:5]
+    user_comments = user.commenttrack_set.all()[:5]
 
-        user_posts = user.post_set.all()[:5]
-        user_comments = user.commenttrack_set.all()[:5]
-
-        context = {
-            "sign_in": SignInForm,
-            "user": user,
-            "user_posts": user_posts,
-            "user_comments": user_comments
-        }
-        return render(request, 'profile/profile.html', context)
-
-    else:
-        context = {
-            "status_code": 404,
-            "message": "Not Found!"
-        }
-        return render(request, 'error.html', context, status=404)
+    context = {
+        "sign_in": SignInForm,
+        "user": user,
+        "user_posts": user_posts,
+        "user_comments": user_comments
+    }
+    return render(request, 'profile/profile.html', context)
 
 
 def follow_toggle(request, pk):
