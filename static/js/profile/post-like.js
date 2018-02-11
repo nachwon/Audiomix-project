01f412acbfe3f9e41f0222c56205541f4806a0c8
Che1 <nachwon@gmail.com>
2018-02-12 00:25:04 +0900

    javascript separated

--- static/js/profile/post-like.js
+++ static/js/profile/post-like.js
@@ -1,30 +1,23 @@
+function like(pk) {
+    var url = "/post/" + pk + "/like/";
 
-$(document).ready(function() {
-    var like_btn_list = $(".like-btn");
-
-    like_btn_list.click(function (){
-        var pk = $(this).attr("data-post-pk");
-        var url = $(this).attr("data-url");
-
-        var counter = $("#like-count-" + pk);
-        var csrf_token = $('[name=csrfmiddlewaretoken]').val();
-        $.ajax({
-            type: "POST",
-            url: url,
-            data: {'pk': pk, 'csrfmiddlewaretoken': csrf_token},
-            async: true,
-            dataType: "json",
-            success: function(response) {
-                counter.html(response.count);
-                var btn = $("#like-btn-" + pk);
-                if (btn[0].className === "like-btn glyphicon glyphicon-heart") {
-                    btn[0].className = "like-btn glyphicon glyphicon-heart-empty"
-                }
-                else {
-                    btn[0].className = "like-btn glyphicon glyphicon-heart"
-                }
+    var counter = $("#like-count-" + pk);
+    var csrf_token = $('[name=csrfmiddlewaretoken]').val();
+    $.ajax({
+        type: "POST",
+        url: url,
+        data: {'pk': pk, 'csrfmiddlewaretoken': csrf_token},
+        async: true,
+        dataType: "json",
+        success: function (response) {
+            counter.html(response.count);
+            var btn = $("#like-btn-" + pk);
+            if (btn[0].className === "like-btn glyphicon glyphicon-heart") {
+                btn[0].className = "like-btn glyphicon glyphicon-heart-empty"
             }
-        })
-    });
-});
-
+            else {
+                btn[0].className = "like-btn glyphicon glyphicon-heart"
+            }
+        }
+    })
+}
