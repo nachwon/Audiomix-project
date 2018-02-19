b9bbc6e85c738888cfecfb50201739047239d76f
nachwon <nachwon@gmail.com>
2018-02-19 17:03:12 +0900

    follow btns hover updated

--- static/js/profile/follow-hover.js
+++ static/js/profile/follow-hover.js
@@ -1,25 +1,20 @@
 $(document).ready(function(){
     $('.follow-hover-up').popover(
         {
-            title: "Header",
-            content: "Blabla",
+            html: true,
             trigger: "manual",
             placement: "top"
         })
         .on("mouseenter", function() {
-            console.log("enter");
             var _this = this;
             $(this).popover("show");
             $(".popover").on("mouseleave", function() {
-                console.log("leave1");
                 $(_this).popover("hide");
             });
         })
         .on("mouseleave", function() {
-            console.log("leave2");
             var _this = this;
             setTimeout(function () {
-                console.log("leave3");
                 if (!$(".popover:hover").length) {
                     $(_this).popover("hide");
                 }
