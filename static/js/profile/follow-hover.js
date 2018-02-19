14beae2e339dcd4679f9a3cc7b3c7f73d7636b08
nachwon <nachwon@gmail.com>
2018-02-19 17:50:08 +0900

    follow popover updated

--- static/js/profile/follow-hover.js
+++ static/js/profile/follow-hover.js
@@ -8,42 +8,41 @@ $(document).ready(function(){
         .on("mouseenter", function() {
             var _this = this;
             $(this).popover("show");
-            // $(".popover").on("mouseleave", function() {
-            //     $(_this).popover("hide");
-            // });
+            $(".popover").on("mouseleave", function() {
+                $(_this).popover("hide");
+            });
         })
-    //     .on("mouseleave", function() {
-    //         var _this = this;
-    //         setTimeout(function () {
-    //             if (!$(".popover:hover").length) {
-    //                 $(_this).popover("hide");
-    //             }
-    //         }, 0);
-    //     });
-    // $('.follow-hover-down').popover(
-    //     {
-    //         title: "Header",
-    //         content: "Blabla",
-    //         trigger: "manual",
-    //         placement: "bottom"
-    //     })
-    //     .on("mouseenter", function() {
-    //         console.log("enter");
-    //         var _this = this;
-    //         $(this).popover("show");
-    //         $(".popover").on("mouseleave", function() {
-    //             console.log("leave1");
-    //             $(_this).popover("hide");
-    //         });
-    //     })
-    //     .on("mouseleave", function() {
-    //         console.log("leave2");
-    //         var _this = this;
-    //         setTimeout(function () {
-    //             console.log("leave3");
-    //             if (!$(".popover:hover").length) {
-    //                 $(_this).popover("hide");
-    //             }
-    //         }, 0);
-    //     });
+        .on("mouseleave", function() {
+            var _this = this;
+            setTimeout(function () {
+                if (!$(".popover:hover").length) {
+                    $(_this).popover("hide");
+                }
+            }, 0);
+        });
+    $('.follow-hover-down').popover(
+        {
+            html: true,
+            trigger: "manual",
+            placement: "bottom"
+        })
+        .on("mouseenter", function() {
+            console.log("enter");
+            var _this = this;
+            $(this).popover("show");
+            $(".popover").on("mouseleave", function() {
+                console.log("leave1");
+                $(_this).popover("hide");
+            });
+        })
+        .on("mouseleave", function() {
+            console.log("leave2");
+            var _this = this;
+            setTimeout(function () {
+                console.log("leave3");
+                if (!$(".popover:hover").length) {
+                    $(_this).popover("hide");
+                }
+            }, 0);
+        });
 });
