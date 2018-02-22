9eec7e84a8b51bac3a558be93e6bfc2fdf8078ff
nachwon <nachwon@gmail.com>
2018-02-22 12:21:18 +0900

    play pause added

--- static/js/profile/comment-waveform.js
+++ static/js/profile/comment-waveform.js
@@ -1,6 +1,17 @@
 var comment_obj = $(".comment-obj");
 
+// 커맨트 트랙 재생/일시정지
 comment_obj.find(".comment-play-icon").on("click", function () {
-   var pk = $(this).attr("data-comment-pk");
-   console.log($("#comment-track-" + pk)[0].play())
+    var pk = $(this).attr("data-comment-pk");
+    var comment_track = $("#comment-track-" + pk);
+    var is_playing = comment_track.attr("data-is-playing");
+
+    if (is_playing === "true") {
+        comment_track[0].pause();
+        comment_track.attr("data-is-playing", "false")
+    }
+    else if (is_playing === "false") {
+        comment_track[0].play();
+        comment_track.attr("data-is-playing", "true")
+    }
 });
