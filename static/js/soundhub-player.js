2e8fb40a40ca626778f40988cd8859fd1c24a333
nachwon <nachwon@gmail.com>
2018-02-26 22:27:13 +0900

    player updated

--- static/js/soundhub-player.js
+++ static/js/soundhub-player.js
@@ -1,4 +1,7 @@
-function getCurrentPlaying() {
+var player_play_btn = $("#player-play-btn");
+
+function getCurrentPlaying(self) {
+    var target_id = $(self).data("target");
     var audios = $(".audio-file");
     var player_total_duration = $("#player-total-duration");
     var player_current_time = $("#player-current-time");
@@ -10,7 +13,27 @@ function getCurrentPlaying() {
             });
             $(item).on("timeupdate", function() {
                 player_current_time.text(format_time(item.currentTime));
-            })
+            });
+
+            player_play_btn.attr("data-target", target_id);
         }
     });
+}
+
+// player_play_btn.on("click", function() {
+//     var target_audio = $("#" + $(this).data("target"));
+//     target_audio[0].pause();
+//     console.log(target_audio)
+// });
+
+function playerBtn(self) {
+    var target_audio = $("#" + $(self).attr("data-target"));
+    if (target_audio.attr("data-isPlaying") === "false") {
+        target_audio[0].play();
+        target_audio.attr("data-isPlaying", "true")
+    }
+    else {
+        target_audio[0].pause();
+        target_audio.attr("data-isPlaying", "false")
+    }
 }
