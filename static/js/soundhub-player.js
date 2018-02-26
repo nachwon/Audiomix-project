5d0db956fc45fcf42c6c19688702e5fb221d1dca
nachwon <nachwon@gmail.com>
2018-02-26 21:46:48 +0900

    connected audios with player

--- static/js/soundhub-player.js
+++ static/js/soundhub-player.js
@@ -1,9 +1,13 @@
 function getCurrentPlaying() {
     var audios = $(".audio-file");
+    var player_current_time = $("#player-current-time");
+
     audios.each(function(index, item){
         if($(item).attr("data-isPlaying") === "true") {
-            console.log(item)
+            $(item).on("play", function() {
+                console.log(item.id + "playing!!");
+                player_current_time.text(format_time(item.duration))
+            })
         }
     });
-
 }
