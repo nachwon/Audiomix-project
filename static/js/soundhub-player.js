c3700c249872c212d1f6b9cca524662a63fade3f
nachwon <nachwon@gmail.com>
2018-02-26 21:50:47 +0900

    connected audios with player

--- static/js/soundhub-player.js
+++ static/js/soundhub-player.js
@@ -1,12 +1,15 @@
 function getCurrentPlaying() {
     var audios = $(".audio-file");
+    var player_total_duration = $("#player-total-duration");
     var player_current_time = $("#player-current-time");
 
     audios.each(function(index, item){
         if($(item).attr("data-isPlaying") === "true") {
             $(item).on("play", function() {
-                console.log(item.id + "playing!!");
-                player_current_time.text(format_time(item.duration))
+                player_total_duration.text(format_time(item.duration))
+            });
+            $(item).on("timeupdate", function() {
+                player_current_time.text(format_time(item.currentTime));
             })
         }
     });
