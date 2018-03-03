4c814ae75e9e5e607011e154e76dd847dd0fd627
nachwon <nachwon@gmail.com>
2018-03-03 20:57:18 +0900

    failed

--- static/js/profile/follow-popover.js
+++ static/js/profile/follow-popover.js
@@ -1,3 +1,24 @@
+$(document).ready(function() {
+    $(".followers-obj")
+        .on("mouseenter", function() {
+            console.log("btn enter");
+            $(this).find(".follow-popover").css("display", "block")
+        })
+        .on("mouseleave", function() {
+            console.log("btn leave");
+            $(this).find(".follow-popover").css("display", "none")
+        });
+
+    $(".psuedo-object-wrapper")
+        .on("mouseenter", function() {
+            console.log("popover enter");
+            $(this).parent().css("display", "block")
+        })
+        .on("mouseleave", function() {
+            console.log("popover leave");
+            $(this).parent().css("display", "none")
+        })
+});
 
 // 팝오버 창에서 팔로우 버튼 ajax 보내기
 function popoverFollow(pk) {
