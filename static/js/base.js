5062d6b213428e7e9285e5b1d16296ac325aaf59
nachwon <nachwon@gmail.com>
2018-03-15 14:29:05 +0900

    css object-fit added to imgs

--- static/js/base.js
+++ static/js/base.js
@@ -10,23 +10,7 @@ String.prototype.format = String.prototype.f = function() {
 
 
 if (!String.prototype.trim) {
-  String.prototype.trim = function () {
-    return this.replace(new RegExp(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g), '');
-  };
-}
-
-function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
+    String.prototype.trim = function () {
+        return this.replace(new RegExp(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g), '');
+    };
 }
