e97c78d4138eb5747e9ed6b1790a362c1f9a1ae0
nachwon <nachwon@gmail.com>
2018-03-27 16:05:35 +0900

    savemastertrack command refactored

--- soundhub/posts/management/commands/savemastertrack.py
+++ soundhub/posts/management/commands/savemastertrack.py
@@ -1,5 +1,6 @@
 import re
 
+from django.core.files.base import ContentFile
 from django.core.files.storage import default_storage as storage
 from django.core.management import BaseCommand
 
@@ -14,7 +15,6 @@ class Command(BaseCommand):
                           help='saves the author track to master track')
 
     def handle(self, *args, **options):
-        url_parser = re.compile(r".*?/media/(.*)[?].*")
         saved_count = 0
         skipped_count = 0
         error_count = 0
@@ -38,21 +38,13 @@ class Command(BaseCommand):
                     skipped_count += 1
 
                 except ValueError:
-                    author_track_dir = post.author_track.url
-                    author_track_file = url_parser.search(author_track_dir).group(1)
-                    author_track = storage.open(author_track_file, 'r')
+                    author_track = post.author_track.read()
+                    waveform_base = post.author_track_waveform_base.read()
+                    waveform_cover = post.author_track_waveform_cover.read()
 
-                    author_track_waveform_base = post.author_track_waveform_base.url
-                    waveform_base_dir = url_parser.search(author_track_waveform_base).group(1)
-                    waveform_base = storage.open(waveform_base_dir, 'r')
-
-                    author_track_waveform_cover = post.author_track_waveform_cover.url
-                    waveform_cover_dir = url_parser.search(author_track_waveform_cover).group(1)
-                    waveform_cover = storage.open(waveform_cover_dir, 'r')
-
-                    post.master_track.save('master_track.mp3', author_track)
-                    post.master_track_waveform_base.save('master_track_waveform_base.png', waveform_base)
-                    post.master_track_waveform_cover.save('master_track_waveform_cover.png', waveform_cover)
+                    post.master_track.save('master_track.mp3', ContentFile(author_track))
+                    post.master_track_waveform_base.save('master_track_waveform_base.png', ContentFile(waveform_base))
+                    post.master_track_waveform_cover.save('master_track_waveform_cover.png', ContentFile(waveform_cover))
 
                     print(f'Post_{post.pk}: Successfully saved author track to master track.')
                     saved_count += 1
