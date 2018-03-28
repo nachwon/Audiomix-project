4ce001e8520e5da4f8464fecf4c4bf4bf25b650d
nachwon <nachwon@gmail.com>
2018-03-28 16:51:39 +0900

    mixer fadder fixed

--- static/js/post-detail/mixer.js
+++ static/js/post-detail/mixer.js
@@ -17,25 +17,23 @@ function loadMixer() {
 
 function connectFader() {
     var fader = $(".fader");
+    var fader_chrome = document.getElementsByClassName("fader");
+
+    // JQuery의 dataTransfer 에러 때문에 바닐라 자바스크립트로 작성
+    for (var i = 0; i < fader_chrome.length; i++) {
+        fader_chrome[i].addEventListener("dragstart", function(e) {
+            e.dataTransfer.setData('Text', e.pageY);
+            $(fader_chrome[i]).css("pointer-events", "none");
+        })
+    }
 
     fader.each(function(index, item) {
-        $(item).on("dragstart", function(e) {
-            console.log(e.pageY);
-            e.originalEvent.dataTransfer.setData('Text', e.pageY);
-            $(item).css("pointer-events", "none")
-        });
-
-        $(item).on("drag", function(e) {
-            console.log(e);
-        });
-
         $(item).on("dragend", function() {
-            $(item).css("pointer-events", "visible")
+            $(item).css("pointer-events", "visible");
         });
 
         var meter_position = $(item).parent().parent().find(".meter-position-getter");
         $(document).on("dragover", function(e) {
-            e.preventDefault();
             var position = e.pageY - meter_position.offset().top;
             if (position > 250) {
                 position = 250
