c9c08af923e3abbb1d314a1ae258f2cd1ba870d0
nachwon <nachwon@gmail.com>
2018-03-28 18:08:06 +0900

    mixer fadder fixed for chrome

--- static/js/post-detail/mixer.js
+++ static/js/post-detail/mixer.js
@@ -1,3 +1,5 @@
+var position_data;
+
 $(document).ready(function() {
     // loadMixer();
     connectFader();
@@ -22,26 +24,39 @@ function connectFader() {
     // JQuery의 dataTransfer 에러 때문에 바닐라 자바스크립트로 작성
     for (var i = 0; i < fader_chrome.length; i++) {
         fader_chrome[i].addEventListener("dragstart", function(e) {
-            e.dataTransfer.setData('Text', e.pageY);
+            e.dataTransfer.setData('text', e.offsetY);
+            position_data = e.offsetY;
             $(fader_chrome[i]).css("pointer-events", "none");
-        })
+        });
     }
+
+    document.addEventListener("dragover", function(e) {
+        for (var i = 0; i < fader_chrome.length; i++) {
+            var data = e.dataTransfer.getData('text');
+            if (data === "") {
+                data = position_data;
+            }
+            setFaderPosition(e, fader_chrome[i], data)
+        }
+    });
 
     fader.each(function(index, item) {
         $(item).on("dragend", function() {
             $(item).css("pointer-events", "visible");
         });
-
-        var meter_position = $(item).parent().parent().find(".meter-position-getter");
-        $(document).on("dragover", function(e) {
-            var position = e.pageY - meter_position.offset().top;
-            if (position > 250) {
-                position = 250
-            }
-            else if (position < 0) {
-                position = 0
-            }
-            $(item).css("top", position + "px")
-        });
     });
+}
+
+function setFaderPosition(e, fader, data) {
+    var meter_position = $(fader).parent();
+
+    var position = parseFloat(e.pageY) - parseFloat(meter_position.offset().top) - parseFloat(data);
+
+    if (position > 250) {
+        position = 250
+    }
+    else if (position < 0) {
+        position = 0
+    }
+    $(fader).css("top", position + "px")
 }
