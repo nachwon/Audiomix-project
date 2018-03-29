09eb16213241c590368b59d5423490fe70b6261e
nachwon <nachwon@gmail.com>
2018-03-29 12:57:11 +0900

    fader refactored

--- static/js/post-detail/mixer.js
+++ static/js/post-detail/mixer.js
@@ -14,8 +14,15 @@ function loadMixer() {
         var source = audioCtx.createMediaElementSource(audio);
 
         connectFader(source, audioCtx);
+        connectPanner(source, audioCtx);
 
     })
+}
+function connectPanner(source, audioCtx) {
+    var panner = audioCtx.createPanner();
+
+    var pan_slider = $(".pan-slider");
+
 }
 
 // 페이더 동작 설정
@@ -25,19 +32,18 @@ function connectFader(source, audioCtx) {
     source.connect(gainNode);
     gainNode.connect(audioCtx.destination);
 
-    var fader = $(".fader");
-    var fader_chrome = document.getElementsByClassName("fader");
+    var fader = document.getElementsByClassName("fader");
 
     // JQuery의 dataTransfer 에러 때문에 바닐라 자바스크립트로 작성
-    for (var i = 0; i < fader_chrome.length; i++) {
+    for (var i = 0; i < fader.length; i++) {
         // 각 페이더에 드레그스타트 이벤트 등록
-        fader_chrome[i].addEventListener("dragstart", function(e) {
-            // 드레그시 Y축 좌표값 전달
+        fader[i].addEventListener("dragstart", function(e) {
+            // 드레그시 페이더 내에서의 Y축 좌표값 전달
             e.dataTransfer.setData('text', e.offsetY);
             // 크롬은 dataTransfer가 작동을 안함. 따라서 전역변수를 이용함.
             position_data = e.offsetY;
             // 드레그시작한 순간 페이더의 포인터 이벤트 제거
-            $(fader_chrome[i]).css("pointer-events", "none");
+            $(fader[i]).css("pointer-events", "none");
 
             // 드레그 고스트이미지 제거
             var crt = document.getElementById("ghost");
@@ -46,11 +52,16 @@ function connectFader(source, audioCtx) {
             // 파이어폭스: 고스트이미지를 화면 밖으로 보냄
             e.dataTransfer.setDragImage(crt, 10000, 10000);
         });
+
+        // 드레그 끝난 경우 포인터 이벤트 원상복귀
+        fader[i].addEventListener("dragend" ,function() {
+            fader[i].style.pointerEvents = "visible";
+        });
     }
 
     // 페이지 전체에 드레그오버 이벤트 등록
     document.addEventListener("dragover", function(e) {
-        for (var i = 0; i < fader_chrome.length; i++) {
+        for (var i = 0; i < fader.length; i++) {
             // 파이어폭스의 경우 getData로 Y축 좌표 가져옴.
             var data = e.dataTransfer.getData('text');
             // 크롬의 경우 전역변수에서 가져옴.
@@ -58,7 +69,7 @@ function connectFader(source, audioCtx) {
                 data = position_data;
             }
             // 페이더 위치에 따른 볼륨 값을 리턴
-            var volume = setFaderPosition(e, fader_chrome[i], data);
+            var volume = setFaderPosition(e, fader[i], data);
             // 볼륨 값 스케일링: 기본값 1
             var gain_value = 1.25 - volume / 200;
             // 믹서에서 보여줄 페이더 값: 기본값 0
@@ -80,12 +91,7 @@ function connectFader(source, audioCtx) {
         }
     });
 
-    // 드레그 끝난 경우 포인터 이벤트 원상복귀
-    fader.each(function(index, item) {
-        $(item).on("dragend", function() {
-            $(item).css("pointer-events", "visible");
-        });
-    });
+
 }
 
 // 페이더 드래그 시 위치 변경
